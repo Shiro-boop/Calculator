767952a7d194f8503c9f19344f20165b462c8994
Alexander <nellishiro@gmail.com>
2025-02-13 14:26:19 +0200

    Add function divide and multiply, also dark/light theme

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let result = document.querySelector('.result__form'),
     resultPast = document.querySelector('.result__past__form')
 const btns = document.querySelector('.buttons'),
     operands = document.querySelectorAll('.operand'),
-    equals = document.querySelector('.equals');
+    equals = document.querySelector('.equals'),
+    darkTheme = document.querySelector('.dark__theme'),
+    lightTheme = document.querySelector('.light__theme');
 
 let firstSum = 0,
     operand,
@@ -25,7 +27,7 @@ btns.addEventListener('click', (e) => {
 
 operands.forEach(item => {
     item.addEventListener('click', (e) => {
-        if(operand === '+' || operand === "-") {
+        if(operand === '+' || operand === "-" || operand === '*' || operand === "/") {
             alert('Поддерживается только одна операция')
         } else {
             firstSum = result.textContent;
@@ -57,5 +59,55 @@ equals.addEventListener('click', (e) => {
         firstSum = 0;
         operand = '';
         secondSum = 0;
+    } else if (operand === '*') {
+        let secondSum = result.textContent.slice(result.textContent.search(/\*/) + 1); // Получаю из result строку, нахожу на какой позиции находится *, отсекаю от строки первую часть с минусом
+
+        result.textContent = +firstSum * +secondSum;
+        resultPast.textContent = `${+firstSum} * ${+secondSum} = ${result.textContent}`;
+
+        firstSum = 0;
+        operand = '';
+        secondSum = 0;
+    } else if (operand === '/') {
+        let secondSum = result.textContent.slice(result.textContent.search(/\//) + 1); // Получаю из result строку, нахожу на какой позиции находится /, отсекаю от строки первую часть с минусом
+
+        result.textContent = (+firstSum / +secondSum).toFixed(2);
+        resultPast.textContent = `${+firstSum} / ${+secondSum} = ${result.textContent}`;
+
+        firstSum = 0;
+        operand = '';
+        secondSum = 0;
     }
+})
+
+darkTheme.addEventListener('click', (e) => {
+    const body = document.querySelector('body'),
+        nav = document.querySelector('nav'); 
+
+    Array.from(btns.children).forEach(item => {
+        item.style.color = 'white';
+        item.style.borderColor = '#f59517';
+    })
+    result.parentElement.style.borderColor = '#f59517'
+    result.style.color = 'white';
+    body.style.background = 'black';
+    nav.style.background = '#f59517';
+    darkTheme.style.display = 'none';
+    lightTheme.style.display = 'block';
+})
+
+lightTheme.addEventListener('click', (e) => {
+    const body = document.querySelector('body'),
+        nav = document.querySelector('nav'); 
+
+    Array.from(btns.children).forEach(item => {
+        item.style.color = 'black';
+        item.style.borderColor = '#ffbef5';
+    })
+    result.parentElement.style.borderColor = '#ffbef5'
+    result.style.color = 'black';
+    body.style.background = 'white';
+    nav.style.background = '#ffbef5';
+    darkTheme.style.display = 'block';
+    lightTheme.style.display = 'none';
 })
